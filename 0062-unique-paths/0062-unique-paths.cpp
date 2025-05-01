be8fc2bac2896106d55ff950ad33424233228a40
Prateekyadav-ai <prateekyadav630738@gmail.com>
2025-05-01 20:25:45 +0530

    Time: 3 ms (8.31%), Space: 9.3 MB (56.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-         vector<vector<int>> arr(m, vector<int>(n, 0));
+        vector<vector<int>> arr(m, vector<int>(n, 0));;
        for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(i==0||j==0){
