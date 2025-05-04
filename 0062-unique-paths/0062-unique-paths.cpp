c5d6666e8513d3e1dee825fa73597ee1843674f6
Prateekyadav-ai <prateekyadav630738@gmail.com>
2025-05-04 16:02:51 +0530

    Time: 0 ms (100%), Space: 7.9 MB (91.74%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> arr(m, vector<int>(n, 0));;
+int bruteforce(int m,int n){
+     vector<vector<int>> arr(m, vector<int>(n, 0));;
        for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(i==0||j==0){
@@ -13,5 +13,21 @@ public:
         }
        }
        return arr[m-1][n-1];
+}
+    int uniquePaths(int m, int n) {
+        int dp[m+1][n+1] ;
+        for(int i=0;i<m;i++){
+            dp[i][0] = 1;
+        }
+        for(int j=0;j<n;j++){
+            dp[0][j] = 1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    
     }
 };
