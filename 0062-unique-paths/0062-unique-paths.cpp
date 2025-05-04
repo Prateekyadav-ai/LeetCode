d22e7701834012cf2db1dcab40828b43b5e8d173
Prateekyadav-ai <prateekyadav630738@gmail.com>
2025-05-04 16:42:46 +0530

    Time: 0 ms (100%), Space: 9.3 MB (32.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-int bruteforce(int m,int n){
-     vector<vector<int>> arr(m, vector<int>(n, 0));;
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(i==0||j==0){
-             arr[i][j]=1;
-            }
-            else{
-                arr[i][j]=arr[i-1][j]+arr[i][j-1];
-            }
+    int countPaths(int i, int j, int m, int n,vector<vector<int>>&dp) {
+        // Base case: reached bottom-right corner
+        if (i == m - 1 && j == n - 1) return 1;
+        
+        // Out of bounds
+        if (i >= m || j >= n) return 0;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-       }
-       return arr[m-1][n-1];
-}
+        
+        // Move down and move right
+        dp[i][j] =countPaths(i + 1, j, m, n,dp) + countPaths(i, j + 1, m, n,dp);
+        return dp[i][j];
+    }
+
     int uniquePaths(int m, int n) {
-        int dp[m+1][n+1] ;
-        for(int i=0;i<m;i++){
-            dp[i][0] = 1;
-        }
-        for(int j=0;j<n;j++){
-            dp[0][j] = 1;
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
-    
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return countPaths(0, 0, m, n,dp);
     }
 };
