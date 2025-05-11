198f4d2448d8fb202230b239801f0e3e24cee0bf
Prateekyadav-ai <prateekyadav630738@gmail.com>
2025-05-11 10:54:48 +0530

    Time: 0 ms (100%), Space: 11.8 MB (59.6%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -3,7 +3,7 @@ public:
     bool threeConsecutiveOdds(vector<int>& nums) {
         int n=nums.size();
         for(int i=0;i<n-2;i++){
-          if((nums[i]%2!=0) && (nums[i+1]%2!=0) && (nums[i+2]%2!=0)){
+          if((nums[i]&1!=0) && (nums[i+1]&1!=0) && (nums[i+2]&1!=0)){
             return true;
           }
           
