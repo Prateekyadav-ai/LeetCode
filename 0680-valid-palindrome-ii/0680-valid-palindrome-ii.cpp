fab687424c82bcea8a0757ee38491e6c642bb08f
Prateekyadav-ai <prateekyadav630738@gmail.com>
2025-08-03 10:52:46 +0530

    Time: 3 ms (71.95%), Space: 26.7 MB (24.46%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
-    bool checkPalindrome(string str, int s, int e) {
-        while(s <= e) {
-            if(str[s] != str[e]) {
+    // Check if a substring is palindrome
+    bool checkPalindrome(string s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j]) {
                 return false;
             }
-            else {
-                s++;
-                e--;
-            }
+            i++;
+            j--;
         }
-        //valid palindrome
         return true;
     }
-    bool validPalindrome(string s) {
-        int len = s.length();
-        int i=0; 
-        int j = len-1;
 
-        while(i <= j) {
-            //same 
-            if(s[i] == s[j]) {
-                i++;
-                j--;
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+
+        while (i < j) {
+            if (s[i] != s[j]) {
+                // Try skipping one character either from left or right
+                return checkPalindrome(s, i + 1, j) || checkPalindrome(s, i, j - 1);
             }
-            //different
-            else {
-                //s[i] != s[j]
-                //iss case me character delete krke dekho
-                //2 options
-                //delete ith index wala character
-                bool ansOne = checkPalindrome(s, i+1, j);
-                if(ansOne == true)
-                    return true;
-                //delete jth index wala character
-                bool ansTwo = checkPalindrome(s,i,j-1);
-                bool finalAns = ansOne || ansTwo;
-                return finalAns;
-            }
+            i++;
+            j--;
         }
-        //valid palindome, without any deletion
         return true;
     }
 };
